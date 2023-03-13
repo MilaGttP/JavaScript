02d34b3ff40b573f06db9d1fde21f2e42dbfaa18
Mila <milaivanova2609@gmail.com>
2023-03-13 21:57:19 +0200

    Task 1 completed

---
+++ js/task1.js
@@ -0,0 +1,8 @@
+
+const name = "Generator";
+let price = 1000;
+
+console.log(`Selected "${name}", price per item is ${price}`);
+
+price = 2000;
+console.log(`Selected "${name}", price per item is ${price}`);
