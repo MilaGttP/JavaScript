763ab5b7b960ea164765782fb2448d8ab8be07e9
Mila <milaivanova2609@gmail.com>
2023-04-05 22:23:53 +0300

    Fourth task completed

---
+++ js/formatString.js
@@ -0,0 +1,23 @@
+
+const formatString = function (string) {
+  if (string.length <= 40) {
+    return string;
+  }
+  return string.slice(0, 40) + '...';
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// повернеться оригінальний рядок
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// повернеться форматований рядок
+
+console.log(formatString('Curabitur ligula sapien.'));
+// повернеться оригінальний рядок
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// повернеться форматований рядок
