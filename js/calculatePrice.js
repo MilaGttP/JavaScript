c3be59d6a74735bf1e3d4b38e960f905410c89b0
Mila <milaivanova2609@gmail.com>
2023-04-05 21:59:17 +0300

    Second task completed

---
+++ js/calculatePrice.js
@@ -0,0 +1,27 @@
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  message = message.split(' ');
+  return message.length * pricePerWord;
+};
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
