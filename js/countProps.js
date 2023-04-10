aa78e01cf51f94bf2278a907f4f530596fc33da8
Mila <milaivanova2609@gmail.com>
2023-04-10 22:42:06 +0300

    Second task completed

---
+++ js/countProps.js
@@ -0,0 +1,10 @@
+
+const countProps = function (obj) {
+    return Object.keys(obj).length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
