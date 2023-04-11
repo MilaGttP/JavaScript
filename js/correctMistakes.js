f9a0f31d172eed36d910b0c61433559766726db8
Mila <milaivanova2609@gmail.com>
2023-04-11 10:11:10 +0300

    Nineth task completed

---
+++ js/correctMistakes.js
@@ -0,0 +1,29 @@
+
+const inventory = {
+    items: ["Knife", "Gas mask"],
+    add(itemName) {
+        console.log(`Adding ${itemName} to inventory`);
+        this.items.push(itemName);
+    },
+    remove(itemName) {
+        console.log(`Removing ${itemName} from inventory`);
+        this.items = this.items.filter((item) => item !== itemName);
+    },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action.call(inventory, itemName);
+};
+
+invokeInventoryAction("Medkit", inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
