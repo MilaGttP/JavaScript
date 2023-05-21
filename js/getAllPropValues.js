b0662410f7a87e22cac231be332701d62fec8504
Mila <milaivanova2609@gmail.com>
2023-05-21 11:24:57 +0300

    Task 5 completed

---
+++ js/getAllPropValues.js
@@ -0,0 +1,17 @@
+
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроїд', price: 400, quantity: 7 },
+    { name: 'Захоплення', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+    return arr.map(obj => obj[prop]).filter(value => value !== undefined);
+};
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроїд', 'Захоплення']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
